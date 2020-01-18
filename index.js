4e535d06fb7d57c18555ad3742d241d94e0b2360
damianjnc <damianjnc@gmail.com>
2020-01-18 07:02:40 +0000

    updated print list data

--- index.js
+++ index.js
@@ -6,4 +6,41 @@ class Node {
 }
 
 const n1 = new Node(100)
-console.log(n1)
+
+class LinkedList {
+  constructor(){
+    this.head = null
+    this.size = 0
+  }
+
+  // Insert first Node
+    insertFirst(data){
+      this.head = new Node(data, this.head)
+    }
+  // Insert last Node
+
+  // Insert at index
+
+  // Get at index
+
+  // Remove at index
+
+  // Clear list
+
+  // Print list data
+  printListData(){
+    let current = this.head
+
+    while(current){
+      console.log(current.data)
+      current = current.next
+    }
+  }
+}
+
+const ll = new LinkedList()
+ll.insertFirst(100)
+ll.insertFirst(200)
+ll.insertFirst(300)
+
+ll.printListData()
